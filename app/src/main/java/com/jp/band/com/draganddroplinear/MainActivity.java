4b54bca108f940649cc53cc2f5ca89b5284d03d0
kai <pavanvenkatesh94@gmail.com>
2016-04-16 20:14:59 +0530

    just push

--- app/src/main/java/com/jp/band/com/draganddroplinear/MainActivity.java
+++ app/src/main/java/com/jp/band/com/draganddroplinear/MainActivity.java
@@ -26,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
     ImageView im;
     Button upload ;
     TextView gen;
-    DragLinearLayout dragLinearLayout;
+    DragLinearLayout dragLinearLayout,dragLinearLayout2,dragLinearLayout3;
     @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,15 +37,20 @@ public class MainActivity extends AppCompatActivity {
          gen = (TextView)findViewById(R.id.textViewUpload);
         findViewById(R.id.container).setOnDragListener(new MyDragListener());
         findViewById(R.id.struct).setOnDragListener(new MyDragListener());
+        findViewById(R.id.struct2).setOnDragListener(new MyDragListener());
+        findViewById(R.id.llr).setOnDragListener(new MyDragListener());
 
 
         im.setOnTouchListener(new MyOnTouchListener());
         findViewById(R.id.content).setOnTouchListener(new MyOnTouchListener());
+        findViewById(R.id.content2).setOnTouchListener(new MyOnTouchListener());
 
 
 
         findViewById(R.id.ivTestOrange).setOnTouchListener(new MyOnTouchListener());
 
+        dragLinearLayout2 = (DragLinearLayout) findViewById(R.id.struct) ;
+        dragLinearLayout3 = (DragLinearLayout) findViewById(R.id.struct2) ;
         dragLinearLayout = (DragLinearLayout) findViewById(R.id.container);
        /* for(int i = 0; i < dragLinearLayout.getChildCount(); i++){
             View child = dragLinearLayout.getChildAt(i);
@@ -133,7 +138,89 @@ public class MainActivity extends AppCompatActivity {
                          }
 
 
-                    }else {
+
+
+                    }else if(v == findViewById(R.id.struct)) {
+                        findViewById(R.id.llr).invalidate();
+
+                        // Dropped, reassign View to ViewGroup
+                        View view = (View) event.getLocalState();
+                        ViewGroup owner = (ViewGroup) view.getParent();
+                        owner.removeView(view);
+                        LinearLayout container = (LinearLayout) v;
+                        //RelativeLayout container = (RelativeLayout) v;
+                        container.addView(view);
+                        view.setVisibility(View.VISIBLE);
+                        ClipData.Item item = event.getClipData().getItemAt(0);
+                        ultimateCode += item.getText().toString();
+
+
+                        /*ImageView  i  = new ImageView(MainActivity.this);
+                        String fname = item.getText().toString();
+                        Log.d("TESTEST", fname);
+                        int id = getResources().getIdentifier(fname, "drawable", getPackageName());
+                        Drawable drawable = getResources().getDrawable(id);
+
+                       // i.setBackground(drawable);
+
+                        dragLinearLayout.addDragView(i,i);
+                        dragLinearLayout.setViewDraggable(i,i);*/
+
+
+
+                        for(int i = 0; i < dragLinearLayout2.getChildCount(); i++){
+                            View child = dragLinearLayout2.getChildAt(i);
+                            // the child will act as its own drag handle
+
+
+                            dragLinearLayout2.setViewDraggable(child, child);
+                        }
+
+
+
+
+
+                }else if(v == findViewById(R.id.struct2)) {
+                    findViewById(R.id.llr).invalidate();
+
+                    // Dropped, reassign View to ViewGroup
+                    View view = (View) event.getLocalState();
+                    ViewGroup owner = (ViewGroup) view.getParent();
+                    owner.removeView(view);
+                    LinearLayout container = (LinearLayout) v;
+                    //RelativeLayout container = (RelativeLayout) v;
+                    container.addView(view);
+                    view.setVisibility(View.VISIBLE);
+                    ClipData.Item item = event.getClipData().getItemAt(0);
+                    ultimateCode += item.getText().toString();
+
+
+                        /*ImageView  i  = new ImageView(MainActivity.this);
+                        String fname = item.getText().toString();
+                        Log.d("TESTEST", fname);
+                        int id = getResources().getIdentifier(fname, "drawable", getPackageName());
+                        Drawable drawable = getResources().getDrawable(id);
+
+                       // i.setBackground(drawable);
+
+                        dragLinearLayout.addDragView(i,i);
+                        dragLinearLayout.setViewDraggable(i,i);*/
+
+
+
+                    for(int i = 0; i < dragLinearLayout3.getChildCount(); i++){
+                        View child = dragLinearLayout3.getChildAt(i);
+                        // the child will act as its own drag handle
+
+
+                        dragLinearLayout3.setViewDraggable(child, child);
+                    }
+
+
+
+
+                }
+                    else  {
 
                         // Dropped, reassign View to ViewGroup
                         View view = (View) event.getLocalState();
@@ -179,6 +266,13 @@ public class MainActivity extends AppCompatActivity {
                         ClipData data3 = ClipData.newPlainText("Label", "starfish");
                         View.DragShadowBuilder shadowBuilder3 = new View.DragShadowBuilder(view);
                         view.startDrag(data3, shadowBuilder3, view, 0);
+                        view.setVisibility(View.INVISIBLE);
+
+                        break;
+                    case R.id.content2:
+                        ClipData data4 = ClipData.newPlainText("Label", "starfish");
+                        View.DragShadowBuilder shadowBuilder4 = new View.DragShadowBuilder(view);
+                        view.startDrag(data4, shadowBuilder4, view, 0);
                         view.setVisibility(View.INVISIBLE);
 
                         break;
